ae9287958f6ffdd090cf07e35039f227bfded52a
Bayan Bennett <16971062+BayanBennett@users.noreply.github.com>
2023-11-26 23:51:40 -0800

    checkpoint - fixed homepage - fixed border colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,25 +4,25 @@ import { H1 } from "@/components/typography/Headings";
 
 const Page = () => (
   <main className="flex flex-col pt-16 justify-center items-center">
-    <section className="flex flex-col border-4 relative w-96 py-6 px-12 h-96">
+    <section className="flex flex-col border-4 relative pt-[3vw] pb-[9vw] px-[3vw]">
       <Door direction="up" position="top" />
       <Link href="/about">
-        <H1>About</H1>
+        <h1 className="text-[4vw] px-[3vw]">About</h1>
       </Link>
       <div className="h-8" />
-      <section className="flex flex-row relative items-center">
-        <aside className="-rotate-90 absolute left-0 pointer-events-none">
-          <h2 className="h-36 ">Portfolio</h2>
+      <section className="px-[3vw] flex flex-row relative items-center">
+        <aside className="-rotate-90 absolute left-[-6vw] pointer-events-none">
+          <h2 className="text-[2.5vw]">Portfolio</h2>
         </aside>
         <div className="flex flex-col">
           <Link href="/academic">
-            <H1>Academic</H1>
+            <h1 className="text-[4vw]">Academic</h1>
           </Link>
           <Link href="/professional">
-            <H1>Professional</H1>
+            <h1 className="text-[4vw]">Professional</h1>
           </Link>
           <Link href="/personal">
-            <H1>Personal</H1>
+            <h1 className="text-[4vw]">Personal</h1>
           </Link>
         </div>
       </section>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,18 +10,10 @@ const config: Config = {
   theme: {
     container: {
       center: true,
-      padding: {
-        DEFAULT: "1rem",
-        sm: "2rem",
-        lg: "4rem",
-        xl: "5rem",
-        "2xl": "6rem",
-      },
     },
     extend: {
       borderColor: ({ theme }) => ({
-        ...theme("colors"),
-        DEFAULT: "colors.gray.500",
+        DEFAULT: theme("colors.neutral.950"),
       }),
     },
   },
