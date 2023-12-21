b32798a08dc368c9c671d85b5c3a0196be5802e4
Bayan Bennett <16971062+BayanBennett@users.noreply.github.com>
2023-12-20 22:04:53 -0800

    fix: adjust "portfolio" positioning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,6 @@ const Page = () => (
   <main className="flex flex-col pt-16 place-items-center">
     <section className="flex flex-col border-4 relative py-[9vw] px-[4vw] justify-center">
       <Door direction="up" position="top" />
-      {/*<Link href="/about">*/}
-      {/*  <h1 className="text-[calc(2vw+24px)] px-[calc(6vw+24px)]">About</h1>*/}
-      {/*</Link>*/}
       <section className="px-[4vw] flex flex-row relative place-items-center">
         <aside className="pointer-events-none">
           <h2
@@ -27,8 +24,8 @@ const Page = () => (
         </div>
       </section>
       <div className="h-8" />
-      <section className="px-[4vw] flex flex-row relative place-items-center">
-        <aside className="pointer-events-none">
+      <section className="px-[4vw] flex flex-row relative place-items-center relative">
+        <aside className="pointer-events-none absolute -left-2">
           <h2
             style={{ writingMode: "vertical-lr", transform: "rotate(180deg)" }}
             className="text-[calc(1.6vw+16px)] font-light"
