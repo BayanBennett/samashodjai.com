2716c22c522c1d7d64740e787b0d2cd9b6284bd4
Bayan Bennett <16971062+BayanBennett@users.noreply.github.com>
2023-12-24 14:37:02 -0800

    feat(Door): support downward facing door

--- src/components/Door.tsx
+++ src/components/Door.tsx
@@ -6,16 +6,25 @@ type DoorProps = {
 };
 
 export const Door: FunctionComponent<DoorProps> = ({ position, direction }) => {
-  const positionClass = position === "top" ? "-top-11" : "-bottom-1";
+  const positionClass =
+    position === "top"
+      ? "-top-11"
+      : direction === "down"
+        ? "-bottom-11"
+        : "-bottom-1";
+
+  const scaleClass = direction === "up" ? "scale-y-100" : "-scale-y-100";
+
   return (
-    <div className={`flex flex-col-reverse absolute -right-1 ${positionClass}`}>
+    <div
+      className={`px-10 flex flex-col-reverse absolute -right-[1px] ${positionClass} ${scaleClass}`}
+    >
       <div className="flex flex-row">
         <div className="w-10 border-t-4 border-white" />
-        <div className="w-10 border-t-4" />
       </div>
-      <div className="flex flex-row">
+      <div className="flex flex-row bg-white">
         <div className="w-10 h-10 border-t-2 border-l-2 rounded-tl-full border-dotted" />
-        <div className="w-10 h-10 border-l-2 rounded-tr-full origin-bottom-left" />
+        <div className="h-10 border-l-2 rounded-tr-full origin-bottom-left" />
       </div>
     </div>
   );
@@ -26,7 +35,7 @@ export const DoorHeader: FunctionComponent<DoorProps & { title?: string }> = ({
   direction,
   title,
 }) => (
-  <h2 className="border-b-4 sticky top-14 h-14 pl-6 bg-white z-10 flex justify-start items-end">
+  <h2 className="border-b-4 sticky top-14 h-14 px-10 bg-white z-20 flex justify-start items-end">
     <Door position={position} direction={direction} />
     {title}
   </h2>
